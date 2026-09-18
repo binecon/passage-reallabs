8eb28e49ec4a476a99364aad8dc851915aaa351f
SC <sc@SCs-MacBook-Air.local>
2026-09-18 19:23:18 +0200

    github config1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://reallabs.passage13.org',
+  site: 'https://binecon.github.io',
+  base: '/passage-reallabs',
 });
